beec1dcd47fbfc16905d86e4f25c491554441112
Shashank V <shashankvijendra@gmail.com>
2024-07-11 12:33:58 +0530

    some more alternative code added

--- Neet-code/array_and_hashing/01_contain_duplicate.py
+++ Neet-code/array_and_hashing/01_contain_duplicate.py
@@ -6,6 +6,15 @@ class Solution:
             return False
         return True
     
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        new_set = set()
+        for i in nums:
+            if i in new_set:
+                return False
+            new_set.add(i)
+        return True
+    
 # Input: nums = [1, 2, 3, 3]
 # Output: true
 
--- Neet-code/array_and_hashing/05_top_k_frequency_elements.py
+++ Neet-code/array_and_hashing/05_top_k_frequency_elements.py
@@ -17,5 +17,24 @@ class Solution:
         sort_data = sorted(d.items(), key=lambda item: item[1], reverse=True)
         return [w for w,o in sort_data[:k]]
 
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
 # Input: nums = [1,2,2,3,3,3], k = 2
 # Output: [2,3]

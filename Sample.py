482d2c44ea1758abf02813be4ee98788b0b7830a
shashank <shashankvijendra@gmail.com>
2025-02-06 23:05:43 +0530

    max polindram

--- Sample.py
+++ Sample.py
@@ -1,11 +1,12 @@
-## Use just for testing purposes 
+def longestPalindrome(s):
+    
+    max_str = ""
+    for i in range(len(s)):
+        for j in range(i+1,len(s)):
+            if s[i:j] == s[i:j][::-1] and len(max_str)<len(s[i:j]):
+                max_str = s[i:j]
+    return max_str 
+        
 
-
-def sample(input, target):
-    for i,j in enumerate(input):
-        if target - j in input:
-            return [i, input.index(target - j)]
-
-lst = [3,2,4]
-target = 9
-print(sample(lst, target))
+s = 'babad'      
+print(longestPalindrome(s))
